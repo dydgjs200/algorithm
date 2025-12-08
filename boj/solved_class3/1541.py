9eb19f7c73e24507258e3b74ecba53ed60dd95ea
dydgjs200 <dydgjs2016@naver.com>
2025-12-08 15:46:02 +0900

    solve 1541

--- boj/solved_class3/1541.py
+++ boj/solved_class3/1541.py
@@ -1,9 +1,13 @@
 # 적절한 괄호로 최소값 만들기
 # 중첩 괄호 고려 필요 x (덧셈, 뺄셈만이여서)
+# 최소 = - 이전의 +값을 모두 더한 뒤 빼줘야함
 
-import re
-
-inp = input()
-num = re.findall(r'\d+', inp)
-operator = re.findall(r'[-+]', inp)
-
+s = input().split('-')
+answer = 0
+for i in range(len(s)):
+    temp_sum = sum(map(int, s[i].split('+')))
+    if i == 0:
+        answer += temp_sum
+    else:
+        answer -= temp_sum
+print(answer)
